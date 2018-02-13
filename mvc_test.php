5fd8007638f49328643725325a3d0159fedeee60
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-14 00:49:19 +0100

    minor changes: optional parameters for request_variables.

--- mvc_test.php
+++ mvc_test.php
@@ -3,7 +3,7 @@
 //$request_variables = $_REQUEST; // get from browser: useful for production.
 //echo http_mvc_test1($request_variables);
 
-function http_mvc_test1($request_variables){
+function http_mvc_test1($request_variables=[]){
     $template_file = 'template1.php';
 
     $id = htmlspecialchars((int)@$request_variables['id']);
