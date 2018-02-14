15759cb56e1092a4ae6d827ecda3976dff7e9467
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-14 05:09:45 +0100

    moved templates to their directory.

--- lib_template.php
+++ lib_template.php
@@ -1,6 +1,6 @@
 <?php
 
-function apply_template($template_file, $template_variables){
+function apply_template_no_prefix($template_file, $template_variables){
     $_ = function ($variable_name) use (&$template_variables){
         return $template_variables[$variable_name];
     };
@@ -9,4 +9,8 @@ function apply_template($template_file, $template_variables){
     $produced_template = ob_get_contents();
     ob_end_clean();
     return $produced_template;
+}
+
+function apply_template($template_file, $template_variables){
+    return apply_template_no_prefix(dirname(__FILE__).'/templates/'.$template_file, $template_variables);
 }
