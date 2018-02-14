a8ba1f0458b3dcc8421d23e1a048f3702a4c50fe
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-14 06:26:50 +0100

    fixed URLs for some hosting conditions.

--- templates/template_wrapper.php
+++ templates/template_wrapper.php
@@ -1,4 +1,4 @@
-<a href="/?u=test1&id=10&message=some text">mvc test 1</a> <a href="/?u=multi&tab=1">mvc multi-view</a> <a href="/?u=api&id=10&message=some text">api test</a>
+<a href="?u=test1&id=10&message=some text">mvc test 1</a> <a href="?u=multi&tab=1">mvc multi-view</a> <a href="?u=api&id=10&message=some text">api test</a>
 
 <hr>
 <?=$_('wrapped_content')?>
