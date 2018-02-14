100a32cb49d75fb791e929c5620c58b049647128
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-14 20:02:04 +0100

    todo_add() input is sanitized.

--- manager/todo_manager.php
+++ manager/todo_manager.php
@@ -21,6 +21,8 @@ function todo_remove($id){
 }
 
 function todo_add($item){
+    $item = trim($item);
+    if($item=='') return;
     $db = new PDO('sqlite:db/todo_db.sqlite');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $db->prepare('INSERT INTO todos (description) VALUES (:description)');
