a966afca83199eb967b991a3e2d3140f40fef584
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-15 14:45:08 +0100

    refactoring: routes for todo in a separete file. default route is now 'todo_list'.

--- routes/_router.php
+++ routes/_router.php
@@ -1,9 +1,12 @@
 <?php
 
-$routes['default'] = 'test1';
+$routes['default'] = 'todo_list';
 $routes['404'] = function($request_variables){
     echo 'not found';
 };
+
+require_once 'routes/routes_todo.php';
+
 $routes['api'] = function($request_variables){
     require_once 'routes/api_test.php';
     echo http_api_test1($request_variables);
@@ -17,31 +20,4 @@ $routes['multi'] = function($request_variables){
     echo http_mvc_multi($request_variables);
 };
 
-// todo
 
-$routes['todo_list'] = function($request_variables){
-    require_once 'manager/todo_manager.php';
-    todo_render();
-};
-$routes['todo_add'] = function($request_variables){
-    require_once 'manager/todo_manager.php';
-    todo_add($request_variables['item']);
-    header("Location: ?u=todo_list");
-};
-$routes['todo_remove'] = function($request_variables){
-    require_once 'manager/todo_manager.php';
-    todo_remove($request_variables['id']);
-    header("Location: ?u=todo_list");
-};
-$routes['todo_detail'] = function($request_variables){
-    require_once 'manager/todo_manager.php';
-    $id = (int)@$request_variables['id'];
-    todo_render_detail($id);
-};
-$routes['todo_update'] = function($request_variables){
-    require_once 'manager/todo_manager.php';
-    $id = (int)@$request_variables['id'];
-    $description = (string)@$request_variables['description'];
-    todo_update($id, $description);
-    header("Location: ?u=todo_list");
-};
---
+++ routes/routes_todo.php
@@ -0,0 +1,32 @@
+<?php
+
+$routes['todo_list'] = function($request_variables){
+    require_once 'manager/todo_manager.php';
+    todo_render();
+};
+
+$routes['todo_add'] = function($request_variables){
+    require_once 'manager/todo_manager.php';
+    todo_add($request_variables['item']);
+    header("Location: ?u=todo_list");
+};
+
+$routes['todo_remove'] = function($request_variables){
+    require_once 'manager/todo_manager.php';
+    todo_remove($request_variables['id']);
+    header("Location: ?u=todo_list");
+};
+
+$routes['todo_detail'] = function($request_variables){
+    require_once 'manager/todo_manager.php';
+    $id = (int)@$request_variables['id'];
+    todo_render_detail($id);
+};
+
+$routes['todo_update'] = function($request_variables){
+    require_once 'manager/todo_manager.php';
+    $id = (int)@$request_variables['id'];
+    $description = (string)@$request_variables['description'];
+    todo_update($id, $description);
+    header("Location: ?u=todo_list");
+};
