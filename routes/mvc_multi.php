c4fb4422dcb7af9241f3117b4de73dcc05bf12c0
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-15 09:55:17 +0100

    moved multi-view files into their assigned directory.

--- routes/mvc_multi.php
+++ routes/mvc_multi.php
@@ -1,11 +1,11 @@
 <?php
 function http_mvc_multi($request_variables){
-    $template_file = 'template_multi';
+    $template_file = 'template_multi/template_multi';
 
     $title = "title";
 
     $tab = htmlspecialchars((int)@$request_variables['tab']);
-    $tab_template_file = ['template_tab_0','template_tab_1','template_tab_2'];
+    $tab_template_file = ['template_multi/template_tab_0','template_multi/template_tab_1','template_multi/template_tab_2'];
 
     require_once 'lib_template.php';
     $tab_content = apply_template($tab_template_file[$tab], []);
