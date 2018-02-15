263e595d4d7feb5755dc0689892df3a45ef6120a
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-15 13:19:05 +0100

    feature: mobile viewport for viewing website on mobile devices.

--- templates/template_wrapper.php
+++ templates/template_wrapper.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <a href="?u=test1&id=10&message=some text">mvc test 1</a> <a href="?u=multi&tab=1">mvc multi-view</a> <a href="?u=api&id=10&message=some text">api test</a> <a href="?u=todo_list">To Do</a>
 
 <hr>
