d07e0ce7f3ce5b28d32b7cbb9d010bc8993db4f0
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-08-15 22:01:13 +0200

    reordering, mostly to remove @ syntax

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
 <?php
-
 $request_variables = $_REQUEST;
 
 try {
     serve_request($request_variables);
-} catch (Exception $e) {
+}catch(Exception $e){
     echo 'Caught exception: ',$e->getMessage(),"\n";
 }
 
 function serve_request($request_variables){
-
     $routes = [];
-
     require_once 'routes/_router.php';
-
-    $route='';
+    $route=null;
     if(isset($request_variables['r']))
         $route=$request_variables['r'];
     else if(isset($_SERVER['PATH_INFO']))
         $route=substr($_SERVER['PATH_INFO'],1);
+    if(!isset($routes[$route]))
+        $route=$routes['404'];
+    else
+        $route=$routes[$route];
 
-        $route = @$routes[$route];
-    if(!$route)
-        $route = @$routes['404'];
-    if($route)
-        $route($request_variables);
+    $route($request_variables);
 }
