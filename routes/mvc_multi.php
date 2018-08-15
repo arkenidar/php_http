1e35d1c0157a8c0f59e676066b04e5417012ac01
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-08-15 21:36:00 +0200

    coding style changes to not trigger exceptions

--- routes/mvc_multi.php
+++ routes/mvc_multi.php
@@ -7,7 +7,7 @@ function http_mvc_multi($request_variables){
 
     $title = "title";
 
-    $tab = htmlspecialchars((int)@$request_variables['tab']);
+    $tab = htmlspecialchars(isset($request_variables['tab'])?(int)$request_variables['tab']:0);
     $tab_template_file = ['template_multi/template_tab_0','template_multi/template_tab_1','template_multi/template_tab_2'];
 
     $tab_content = apply_template($tab_template_file[$tab], []);
--- routes/mvc_test.php
+++ routes/mvc_test.php
@@ -5,14 +5,14 @@ require_once 'templates/lib_template.php';
 function http_mvc_test1($request_variables){
     $template_file = 'template1';
 
-    $id = htmlspecialchars((int)@$request_variables['id']);
+    $id = htmlspecialchars(isset($request_variables['id'])?(int)$request_variables['id']:0);
     $title = "title: id=$id";
     $dump = "id: $id";
 
-    if(''==((string)@$request_variables['message']))
+    if(!isset($request_variables['message']))
         $request_variables['message']='(no message)';
 
-    $message = (string)@$request_variables['message'];
+    $message = $request_variables['message'];
     $content = "message: '$message'";
 
     $template_variables = compact('title', 'dump', 'content');
