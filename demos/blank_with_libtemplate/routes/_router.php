8c779b91970b447ba8ad54a8a3e10d68ac581ec2
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-10-27 13:20:12 +0200

    updated: demos/blank_with_libtemplate

--- demos/blank_with_libtemplate/routes/_router.php
+++ demos/blank_with_libtemplate/routes/_router.php
@@ -1,7 +1,6 @@
 <?php
-$routes['default']='blank_with_template';
-$routes['blank_with_template']=function(){
-    require_once 'lib_template.php';
+$routes['']=function(){
+    require_once 'templates/lib_template.php';
     $template_file = 'blank';
     echo apply_template($template_file, ['var'=>123]);
 };
